1343a977b4940db74dfef886ccba29fb5f380cf3
Daniel Augusto Veronezi Salvador <38945620+GutoVeronezi@users.noreply.github.com>
2022-02-24 16:03:13 -0300

    Enable internal links and fix HTML encoding (#2)
    Co-authored-by: GutoVeronezi <daniel@scclouds.com.br>

--- md2pdf_converter.py
+++ md2pdf_converter.py
@@ -12,7 +12,7 @@ print(f"Reading MD files: {list_of_files}.")
 full_md = "\n".join([open(file_name, "r").read() for file_name in list_of_files])
 
 print("Converting content to HTML.")
-html = f'<!DOCTYPE html><html><head><meta charset="utf-8"></head><body>{markdown.markdown(full_md)}<body></html>'
+html = markdown.markdown(full_md)
 
 output_name = sys.argv[1]
 output_file = os.path.join(dir_name, output_name)
@@ -21,5 +21,10 @@ if os.path.exists(output_file):
     print(f"Removing old output file [{output_name}].")
     os.remove(output_file)
 
-print(f"Convering HTML to PDF.")
-pdfkit.from_string(html, output_file)
+options = {
+    'encoding': "UTF-8",
+    'enable-internal-links': '',
+}
+
+print(f"Converting HTML to PDF.")
+pdfkit.from_string(html, output_file, options, verbose=True)
